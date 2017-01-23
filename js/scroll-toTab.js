f83d48de2baa4640103b4a3a77a2534e23288658
José Rasmussen <j.l.rasmussengarcia@gmail.com>
2017-01-24 00:34:32 +0100

    trying to get sect 2 to stick

--- js/scroll-toTab.js
+++ js/scroll-toTab.js
@@ -17,21 +17,39 @@ function scrollToTab() {
 }
 
 
-// /*This hides or shows tab text by scrolling*/
-// $(document).ready(function() { 
-//     var homeSectionContent = $(".homeContent");
-//     var portfolioSectionContent = $(".portfolioContent");
+/*This hides or shows tab text by scrolling*/
+$(document).ready(function() {
+    var homeSectionContent = $(".homeContent");
+    var portfolioSectionContent = $(".portfolioContent");
 
-//     $(window).scroll(function(e) {
-//     var windowPosition = $(document).scrollTop();
+    $(window).scroll(function(e) {
+        var windowPosition = $(document).scrollTop();
 
-<<<<<<< HEAD
+
+        console.log(windowPosition);
+        if (windowPosition >= 500) {
+            homeSectionContent.hide(1000);
+        } else {
+            homeSectionContent.show(1000);
+        }
+
+        if (windowPosition >= 1487 || windowPosition <= 1200) {
+            portfolioSectionContent.hide(500);
+        } else {
+            portfolioSectionContent.show(500);
+        }
+    });
+});
+
 //     console.log(windowPosition);
 //         if (windowPosition >= 500) {
 //             homeSectionContent.hide(1000);
 //         }else{
 //             homeSectionContent.show(1000);
 //         }
+
+//         if (windowPosition >= 1456 || windowPosition <= 1330) {
+
 
 //         if (windowPosition >= 1487 || windowPosition <= 1200) {
 //             portfolioSectionContent.hide(500);
@@ -40,25 +58,3 @@ function scrollToTab() {
 //         }
 //     });
 // });
-=======
-    console.log(windowPosition);
-        if (windowPosition >= 500) {
-            homeSectionContent.hide(1000);
-        }else{
-            homeSectionContent.show(1000);
-        }
-<<<<<<< HEAD
-/*
-        if (windowPosition >= 1456 || windowPosition <= 1330) {
-=======
-
-        if (windowPosition >= 1487 || windowPosition <= 1200) {
->>>>>>> 6f7a166596a501cec8a1b87fea1c111a32dcd1ea
-            portfolioSectionContent.hide(500);
-        } else {
-            portfolioSectionContent.show(500);
-        }
-        */
-    });
-}); 
->>>>>>> aa798f1348fdd369b7596e84c0a83ad6e1f0b803
